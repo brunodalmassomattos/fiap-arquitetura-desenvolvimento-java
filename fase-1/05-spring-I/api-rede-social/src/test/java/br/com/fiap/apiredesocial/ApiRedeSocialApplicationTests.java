d13d17b91155d377071079786a6d0ff37372a580
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-04-19 10:51:46 -0300

    build: commit init

---
+++ fase-1/05-spring-I/api-rede-social/src/test/java/br/com/fiap/apiredesocial/ApiRedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apiredesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
