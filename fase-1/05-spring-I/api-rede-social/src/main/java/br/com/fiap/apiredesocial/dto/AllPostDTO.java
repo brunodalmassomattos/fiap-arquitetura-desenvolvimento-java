0f06f9c9937a86cdb0d080849ccf5b15f6015feb
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-04-29 12:20:07 -0300

    refactor: modify to streams

--- fase-1/05-spring-I/api-rede-social/src/main/java/br/com/fiap/apiredesocial/dto/AllPostDTO.java
+++ fase-1/05-spring-I/api-rede-social/src/main/java/br/com/fiap/apiredesocial/dto/AllPostDTO.java
@@ -1,31 +1,24 @@
 package br.com.fiap.apiredesocial.dto;
 
-import br.com.fiap.apiredesocial.domain.post.Comment;
 import br.com.fiap.apiredesocial.domain.post.Post;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record AllPostDTO(String id, String title, String content, Integer likes, UserDTO user, List<TagDTO> tags,
                          List<CommentDTO> comments) {
 
     public static List<AllPostDTO> fromGetAll(List<Post> allPost) {
-        List<AllPostDTO> allPostDTOS = new ArrayList<>();
-
-        AllPostDTO allPostDTO;
-        for (Post post : allPost) {
-            allPostDTO = new AllPostDTO(
-                    post.getId(),
-                    post.getTitle(),
-                    post.getContent(),
-                    post.getLikes(),
-                    new UserDTO(post.getUser().getId(), post.getUser().getName(), post.getUser().getEmail(), null),
-                    TagDTO.toTagDTO(post.getTags()),
-                    CommentDTO.toCommentsDTO(post.getComments())
-            );
-            allPostDTOS.add(allPostDTO);
-        }
-
-        return allPostDTOS;
+        return allPost.stream()
+                .map(post -> new AllPostDTO(
+                        post.getId(),
+                        post.getTitle(),
+                        post.getContent(),
+                        post.getLikes(),
+                        new UserDTO(post.getUser().getId(), post.getUser().getName(), post.getUser().getEmail(), null),
+                        TagDTO.toTagDTO(post.getTags()),
+                        CommentDTO.toCommentsDTO(post.getComments())
+                ))
+                .collect(Collectors.toList());
     }
 }
